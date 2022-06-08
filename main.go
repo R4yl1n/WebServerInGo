d966cb1f3c3dfdc4dc36eb1c1990cafcd093d7a0
R4yl1n <92576889+R4yl1n@users.noreply.github.com>
2022-06-08 21:19:28 +0200

    rework

--- main.go
+++ main.go
@@ -6,39 +6,12 @@ import (
 	"net/http"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(w, "hello")
-}
-
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
-
-	fmt.Println("Starting server at port 8000")
-	err := http.ListenAndServe(":8000", nil)
+	fmt.Println("Starting server at port 80")
+	log.Println("Starting http://localhost:80")
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ static/js/kacke.js
@@ -0,0 +1,16 @@
+function lighttheme(){
+    document.getElementById("demo").style.color = "white"
+    document.getElementById("demop").style.color = "white"
+    document.getElementById("demop2").style.color = "white"
+    document.getElementById("bodyb").style.backgroundColor = "black"
+    document.getElementById("btn btn-outline-dark").style.color = "white"
+    
+
+}
+function darktheme(){
+    document.getElementById("demo").style.color = "black"
+    document.getElementById("demop").style.color = "black"
+    document.getElementById("demop2").style.color = "black"
+    document.getElementById("bodyb").style.backgroundColor = "white"
+    document.getElementById("btn btn-outline-dark").style.color = "black"
+}
